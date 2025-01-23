9c3c91a46c09e6ec44d5ed0de08e708e0c0e705c
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-22 19:53:48 -0600

    Añadir la accion de insertar estudiante en el usuario y estudiante

--- src/clases/jefe.php
+++ src/clases/jefe.php
@@ -16,21 +16,57 @@ class Jefe
         for ($i = 1; $i <= $carreraGrupos["cantidad"]; $i++) {
 
             $grupo = $indiceGrupo . '0' . $i;
-            echo "<option value=\  $grupo  \>  $grupo </option>";
+            echo "<option value='$grupo'>  $grupo </option>";
         }
     }
 
-    public function realizarOperacionFormAñadirEstudiantes($conexion, $matricula, $contraseña, $id_rol, $nombre, $apellidos, $correo, $id_modalidad, $id_carrera)
+    public function realizarOperacionFormAñadirEstudiantes($conexion, $matricula, $contraseña, $rol, $nombre, $apellidos, $correo, $id_modalidad, $id_carrera, $grupo)
     {
 
         $archivo_cargado = isset($_FILES['archivo_csv']) && $_FILES['archivo_csv']['error'] === UPLOAD_ERR_OK;
         $archivo_tiene_contenido = $archivo_cargado && $_FILES['archivo_csv']['size'] > 0;
 
-        $campos_completos = !empty($id) && !empty($nombre) && !empty($apellidos) && !empty($correo);
+        $campos_completos = !empty($matricula) && !empty($contraseña) && !empty($nombre) && !empty($apellidos) && !empty($correo) && !empty($id_modalidad) && !empty($id_carrera) && !empty($rol) && !empty($grupo);
 
         $opcion = validacionCamposYArchivoCSV($campos_completos, $archivo_tiene_contenido);
 
-        $opcion === 'Campos' ? 1 : 1;
-        $opcion === "Archivo" ? 2 : 2;
+        $opcion === 'Campos' ? $this->operacionInsertarEstudiante($conexion, $matricula, $contraseña, $rol, $nombre, $apellidos, $correo, $id_modalidad, $id_carrera, $grupo) : '';
+        $opcion === "Archivo" ? $this->añadirPorCSVEstudiantes($conexion) : '';
+    }
+
+    public function operacionInsertarEstudiante($conexion, $matricula, $contraseña, $rol, $nombre, $apellidos, $correo, $id_modalidad, $id_carrera, $grupo)
+    {
+
+        if (restriccionKeyDuplicadaEstudiante($matricula, $correo, $conexion)) {
+            return;
+        }
+
+        mysqli_begin_transaction($conexion);
+
+        try {
+
+            if (insertarUsuario($conexion, $matricula, $contraseña, $correo, $rol)) {
+
+
+
+
+                if (!insertarEstudiante($conexion, $matricula, $nombre, $apellidos, $id_carrera, $id_modalidad, $grupo)) {
+                    estructuraMensaje("Ocurrio un problema con la BD", "../../assets/iconos/ic_error.webp", "--rojo");
+                }
+                mysqli_commit($conexion);
+
+                estructuraMensaje("Registro del estudiante exitoso", "../../assets/iconos/ic_correcto.webp", "--verde");
+                return;
+
+            }
+        } catch (Exception $e) {
+            estructuraMensaje("Error al añadir el registro", "../../assets/iconos/ic_error.webp", "--rojo");
+        }
+
+
+    }
+
+    public function añadirPorCSVEstudiantes($conexion)
+    {
     }
 }
