2fa0668f3660a779bb7cbab927f856846fa107eb
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-23 20:55:08 -0600

    Estructure la funcion para que dependiendo del rol mande la data correspondiente

--- src/query/getInfoPersonal.php
+++ src/query/getInfoPersonal.php
@@ -20,37 +20,48 @@ if (isset($_POST['id'])) {
         $nombre = $dataAdministrador[Variables::CAMPO_NOMBRE];
         $apellidos = $dataAdministrador[Variables::CAMPO_APELLIDOS];
 
-        $data = [
-            "identificador" => $idUser,
-            "nombre" => $nombre,
-            "apellidos" => $apellidos,
-            "carrera" => "null",
-            "cargo" => $rol,
-            "correo" => $correo
-        ];
 
-        echo json_encode($data);
+
+        echo json_encode(crearDataInformacionAdministrador($idUser, $nombre, $apellidos, $rol, $correo));
     } else if ($rol === "Jefe de Carrera") {
         $dataJefe = getResultDataTabla($conexion, Variables::TABLA_BD_JEFE, Variables::CAMPO_CLAVE_EMPLEADO_JEFE, campo: $id);
         $nombre = $dataJefe[Variables::CAMPO_NOMBRE];
         $apellidos = $dataJefe[Variables::CAMPO_APELLIDOS];
         $carrera = getResultCarrera($conexion, $dataJefe[Variables::CAMPO_ID_CARRERA]);
 
-        $data = [
-            "identificador" => $idUser,
-            "nombre" => $nombre,
-            "apellidos" => $apellidos,
-            "carrera" => $carrera,
-            "cargo" => $rol,
-            "correo" => $correo
-        ];
-        echo json_encode($data);
+
+        echo json_encode(crearDataInformacionJefe($idUser, $nombre, $apellidos, $rol, $carrera, $correo));
 
     } else {
         echo json_encode(["error" => "ID no proporcionado"]);
     }
+}
 
 
+function crearDataInformacionAdministrador($idUser, $nombre, $apellidos, $rol, $correo)
+{
+    $data = [
+        "clave_empleado" => $idUser,
+        "nombre" => $nombre,
+        "apellidos" => $apellidos,
+        "rol" => $rol,
+        "correo" => $correo
+    ];
 
+    return $data;
+}
+
+function crearDataInformacionJefe($idUser, $nombre, $apellidos, $rol, $carrera, $correo)
+{
+    $data = [
+        "clave_empleado" => $idUser,
+        "nombre" => $nombre,
+        "apellidos" => $apellidos,
+        "carrera" => $carrera,
+        "rol" => $rol,
+        "correo" => $correo
+    ];
+
+    return $data;
 }
 
