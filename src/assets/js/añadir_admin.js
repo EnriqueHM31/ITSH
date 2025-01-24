e58b5898db86352bd0210c6f697d2676336f1c86
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-23 19:26:27 -0600

    Elimine el archivo que no se ocupa

--- src/assets/js/añadir_admin.js
+++
@@ -1,8 +0,0 @@
-var archivoInput = document.getElementById("archivo");
-
-
-archivoInput.addEventListener("change", () => {
-    var archivoNombre = archivoInput.files[0] ? archivoInput.files[0].name : "Ningún archivo seleccionado";
-
-    document.getElementById("nombreArchivo").textContent = archivoNombre
-})
